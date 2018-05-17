16237d57b79a24bb484a617192452f0cfc9ab90a
zhangqian00 <zq_674360351@163.com>
2018-05-17 11:46:21 +0800

    添加侧边栏路由功能

--- src/components/public/aside.vue
+++ src/components/public/aside.vue
@@ -6,12 +6,12 @@
 			</div>
 			<p class="username">admin</p>
 		</div>
-	    <el-menu :default-active="this.$props['isChose']" text-color="#fff" background-color="#2F4050" active-text-color="#ffd04b" class="el-menu-vertical-demo" router="true">
-	      <el-menu-item index="1">
+	    <el-menu :default-active="this.$props['isChose']" text-color="#fff" background-color="#2F4050" active-text-color="#ffd04b" class="el-menu-vertical-demo" router>
+	      <el-menu-item index="1" route="/">
 	        <i class="el-icon-menu"></i>
 	        <span slot="title">首页</span>
 	      </el-menu-item>
-	      <el-menu-item index="2">
+	      <el-menu-item index="2" route="">
 	        <i class="el-icon-search"></i>
 	        <span slot="title">分类管理</span>
 	      </el-menu-item>
@@ -20,18 +20,18 @@
 	          <i class="el-icon-location"></i>
 	          <span>讲师管理</span>
 	        </template>
-	        <el-menu-item index="3-1">选项1</el-menu-item>
-	        <el-menu-item index="3-2">选项2</el-menu-item>
-	        <el-menu-item index="3-3">选项3</el-menu-item>
+	        <el-menu-item index="3-1" route="">选项1</el-menu-item>
+	        <el-menu-item index="3-2" route="">选项2</el-menu-item>
+	        <el-menu-item index="3-3" route="">选项3</el-menu-item>
 	      </el-submenu>
 	      <el-submenu index="4">
 	        <template slot="title">
 	          <i class="el-icon-setting"></i>
 	          <span>课程管理</span>
 	        </template>
-	        <el-menu-item index="4-1">选项1</el-menu-item>
-	        <el-menu-item index="4-2">选项2</el-menu-item>
-	        <el-menu-item index="4-3">选项3</el-menu-item>
+	        <el-menu-item index="4-1" route="">选项1</el-menu-item>
+	        <el-menu-item index="4-2" route="">选项2</el-menu-item>
+	        <el-menu-item index="4-3" route="">选项3</el-menu-item>
 	      </el-submenu>
 	    </el-menu>
 	  </el-col>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import HelloWorld from '@/components/HelloWorld';
 import Home from '../components/home/home.vue'; // 首页
 import UserCenter from '../components/userCenter/userCenter.vue'; // 个人中心
 
@@ -8,7 +7,6 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    // {path: '/',name: 'HelloWorld',component: HelloWorld},
     {name: 'default',path: '/',redirect: {name: 'home'}}, // 重定向
     {name: 'home',path: '/home',component: Home}, // 首页
     {name: 'userCenter',path: '/userCenter',component: UserCenter}, // 个人中心
